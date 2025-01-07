eb4607bff4f477d22cd238ca2b0fbfaa6ea4583b
BaioSbubens <baietti723@gamil.com>
2025-01-07 17:04:58 +0100

    Motif Proten_ids Fixed

--- motifs.py
+++ motifs.py
@@ -2,11 +2,21 @@ import re
 import csv
 from Bio import SeqIO
 import ast
+import pandas as pd
 
-# TODO : the mobidb_lite_swissprot.csv file has the first entry as the column descriptions, did you also read that ? else we
-# TODO : should just adjust the .csv such that column names are smth else and the first entry is the actual first entry
+def load_protein_ids(psiblast_file, hmm_file, e_threshold=0.001):
+    """Load protein IDs from PSI-BLAST and HMM search results."""
+    psiblast_df = pd.read_csv(psiblast_file)
+    hmm_df = pd.read_csv(hmm_file)
+    
+    filtered_hmm_proteins = hmm_df[hmm_df['E-value'] <= e_threshold]['uniprot_id']
+    print(filtered_hmm_proteins)
+    psiblast_proteins = set(psiblast_df['uniprot_id'])
+    hmm_proteins = set(filtered_hmm_proteins)
+    
+    return psiblast_proteins.union(hmm_proteins)
 
-def load_disordered_regions(mobidb_file, pfam_ids):
+def load_disordered_regions(mobidb_file, protein_ids):
     """ Fidnthe disordered regions of the proteins in our family to do the analysis on"""
     disordered_regions = {}
     with open(mobidb_file, "r") as file:
@@ -14,19 +24,18 @@ def load_disordered_regions(mobidb_file, pfam_ids):
         for row in reader:
             protein_id = row[0]
             # If protein found in family, save its disordered regions
-            if protein_id in pfam_ids: # TODO : not pfam_ids ? 
+            if protein_id in protein_ids: #
                 parsed_list = ast.literal_eval(row[1]) # use literal_eval bc the areas are saved as nested lists [[...]] , and we want to parse it as these lists 
                 disordered_regions[protein_id] = [tuple(pair) for pair in parsed_list] # turn each list (i.e. disordered region area) to tuple 
     return disordered_regions
 
-def load_sequences(fasta_file):
+def load_sequences(fasta_file, protein_ids):
     sequences = {}
     for record in SeqIO.parse(fasta_file, "fasta"):
         protein_id = record.id.split("|")[1]
-        sequences[protein_id] = str(record.seq)
+        if protein_id in protein_ids:
+            sequences[protein_id] = str(record.seq)
     return sequences
-
-
 
 def load_motifs(elm_file, prosite_file):
     motifs = {}
@@ -49,7 +58,6 @@ def load_motifs(elm_file, prosite_file):
                 ac, pattern = parts # split into the two parts (i.e. the name and the pattern)
                 motifs[ac] = pattern
     return motifs
-
 
 def match_motifs(sequences, disordered_regions, motifs):
     # in the sequences found in the family that have a disordered region (or we atleast found one), now look into the disordered regions and see if we find any motifs
@@ -93,14 +101,16 @@ def save_results(results, output_file):
 
 def main():
     mobidb_file = "mobidb_lite_swissprot.csv"
-    fasta_file = "uniprotkb_xref_pfam_pf00151_AND_reviewe_2024_12_31.fasta" # TODO : ? why not union of proteins found by HMM & PSSM ? 
+    fasta_file = "uniprot_sprot.fasta"
     elm_file = "elm_classes.tsv"
     prosite_file = "prosite_preprocessed.txt"
+    psiblast_file = "psiblast_parsed.csv"
+    hmm_file = "hmmsearch_output.csv"
     output_file = "conserved_motifs_in_disorder.txt"
 
-    sequences = load_sequences(fasta_file)
-    pfam_ids = set(sequences.keys())
-    disordered_regions = load_disordered_regions(mobidb_file, pfam_ids)
+    protein_ids = load_protein_ids(psiblast_file, hmm_file)
+    sequences = load_sequences(fasta_file, protein_ids)
+    disordered_regions = load_disordered_regions(mobidb_file, protein_ids)
     motifs = load_motifs(elm_file, prosite_file)
 
     results = match_motifs(sequences, disordered_regions, motifs)
